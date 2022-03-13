d7945fa644d29ff3dc7f760f2d0b5ede39ec41e5
insanitywholesale <popomastor@gmail.com>
2022-03-13 16:17:42 +0200

    add basic project files from `dotnet new webapi` with tweaks

---
+++ Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Make route urls and query strings lowercase
+builder.Services.AddRouting(options => {
+	options.LowercaseUrls = true;
+	options.LowercaseQueryStrings = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+} else
+{
+    // Moved here because we don't need HTTPS in development
+    app.UseHttpsRedirection();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
