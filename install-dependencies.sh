4b758a68566d60c2bebce8d6e5d2c101105bacc6
insanitywholesale <popomastor@gmail.com>
2022-03-21 16:39:28 +0200

    add controller generation dependencies to dependency install script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,10 +3,13 @@
 # for modelscontext.cs
 dotnet add package Microsoft.EntityFrameworkCore
 
+# for controller generation
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
+dotnet add package Microsoft.EntityFrameworkCore.Design
+
+dotnet tool install -g dotnet-aspnet-codegenerator
+
 #future stuff. shhhh...
-#dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
-#dotnet add package Microsoft.EntityFrameworkCore.Design
 #dotnet add package Microsoft.EntityFrameworkCore.InMemory
 #dotnet tool install -g dotnet-format
-#dotnet tool install -g dotnet-aspnet-codegenerator
 #dotnet tool install -g Microsoft.dotnet-httprepl
