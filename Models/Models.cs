81e36bc3cde199e4e15068d7a290697678153c04
insanitywholesale <popomastor@gmail.com>
2022-03-24 16:58:29 +0200

    small (hopefully) non-breaking changes, need to regen controllers later

--- Models/Models.cs
+++ Models/Models.cs
@@ -1,4 +1,4 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace adopse_2021.Models
 {
@@ -24,18 +24,25 @@ namespace adopse_2021.Models
 	{
 		public long Id { get; set; }
 		public string? Title { get; set; }
+
 		public virtual ICollection<EvaluationQuestion>? Questions { get; set; }
 	}
 
 	public class EvaluationEvent
 	{
 		public long Id { get; set; }
+
+		[DataType(DataType.Date)]
 		public DateTime? Date { get; set; }
+
 		public bool Active { get; set; }
 		public bool Passed { get; set; }
 		public bool Completed { get; set; }
+		
 		public Evaluation? Evaluation { get; set; }
-		public virtual ICollection<Evaluator>? Evaluators { get; set; }
+
+		public Evaluator? Evaluator { get; set; }
+	
 		public virtual ICollection<Evaluee>? Evaluees { get; set; }
 	}
 
@@ -43,46 +50,42 @@ namespace adopse_2021.Models
 	// https://stackoverflow.com/questions/66772647/how-to-deserialize-a-json-array-containing-objects-having-different-shape-in-c
 	public class EvaluationQuestion
 	{
-		string? Type { get; }
 		public long Id { get; set; }
 		public string? Heading { get; set; }
 	}
 
 	public class EvaluationAnswer
 	{
-		string? Type { get; }
 		public long Id { get; set; }
 		public bool IsCorrectAnswer { get; set; }
 	}
 
 	public class OpenQuestion : EvaluationQuestion
 	{
-		public string Type => "Open";
 		public OpenAnswer? Answer { get; set; }
 	}
 
 	public class OpenAnswer : EvaluationAnswer
 	{
-		public string Type => "Open";
 		public string? ContentFromEvaluee { get; set; }
 	}
 
 	public class MultipleChoiceQuestion : EvaluationQuestion
 	{
-		public string Type => "MultipleChoice";
 		public bool HasCorrectAnswer { get; set; }
 		public ICollection<MultipleChoiceAnswer>? Answers { get; set; }
 	}
 
 	public class MultipleChoiceAnswer : EvaluationAnswer
 	{
-		public string Type => "MultipleChoice";
 		public string? Content { get; set; }
 		public bool SelectedByEvaluee { get; set; }
 	}
 
-	public class Q : EvaluationQuestion
+	public class Q
 	{
+		public long Id { get; set; }
+
 		public ICollection<OpenQuestion>? OpenQuestions { get; set; }
 		public ICollection<MultipleChoiceQuestion>? MultipleChoiceQuestions { get; set; }
 	}
