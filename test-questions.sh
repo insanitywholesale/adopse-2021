85016767455167630f55e33c46dd14ad4c012d5f
insanitywholesale <popomastor@gmail.com>
2022-03-28 01:41:04 +0300

    extend test script

--- test-questions.sh
+++ test-questions.sh
@@ -1,21 +1,61 @@
 #!/bin/sh
 
-curl -X 'POST' \
+curl -s -X 'POST' \
   'http://localhost:5000/api/openquestion' \
   -H 'accept: text/plain' \
   -H 'Content-Type: application/json' \
   -d '{
-  "id": 0,
   "heading": "say hello world",
   "answer": {
-    "id": 0,
     "isCorrectAnswer": true,
     "contentFromEvaluee": "hello world"
   }
-}'
+}' | jq
 
-sleep 3
-
-curl -X 'GET' \
+curl -s -X 'GET' \
   'http://localhost:5000/api/openquestion' \
-  -H 'accept: text/plain'
+  -H 'accept: text/plain' | jq
+
+curl -s -X 'POST' \
+  'http://localhost:5000/api/multiplechoicequestion' \
+  -H 'accept: text/plain' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "heading": "never gonna give you up",
+  "hasCorrectAnswer": true,
+  "answers": [
+    {
+      "isCorrectAnswer": true,
+      "content": "never gonna let you down",
+      "selectedByEvaluee": false
+    },
+    {
+      "isCorrectAnswer": false,
+      "content": "never run around and desert you",
+      "selectedByEvaluee": true
+    },
+    {
+      "isCorrectAnswer": false,
+      "content": "never gonna make you cry",
+      "selectedByEvaluee": false
+    },
+    {
+      "isCorrectAnswer": false,
+      "content": "never gonna say goodbye",
+      "selectedByEvaluee": false
+    },
+    {
+      "isCorrectAnswer": false,
+      "content": "never gonna tell a lie and hurt you",
+      "selectedByEvaluee": false
+    }
+  ]
+}' | jq
+
+curl -s -X 'GET' \
+  'http://localhost:5000/api/multiplechoicequestion' \
+  -H 'accept: text/plain' | jq
+
+curl -s -X 'GET' \
+  'http://localhost:5000/api/q' \
+  -H 'accept: text/plain' | jq
