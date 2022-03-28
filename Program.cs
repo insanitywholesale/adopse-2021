2c90c3a5117f3e839a2e314ded37817653d15af2
insanitywholesale <popomastor@gmail.com>
2022-03-28 10:12:59 +0300

    use postgres if env var is set

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-using Microsoft.EntityFrameworkCore;
+﻿using System.Environment;
 using adopse_2021.Models;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,15 +9,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<EvaluationContext>(options =>
-{
-	options.UseInMemoryDatabase("EvaluationRepo");
-	//options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection");
-});
+// Choose between PostgreSQL and In-
+var usePostgres = System.Environment.GetEnvironmentVariable("USE_POSTGRES")
+
+if usePostgres != "" {
+	builder.Services.AddDbContext<EvaluationContext>(options =>
+	{
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection");
+	});
+} else {
+	builder.Services.AddDbContext<EvaluationContext>(options => {
+		options.UseInMemoryDatabase("EvaluationRepo");
+	});
+}
 
 // Make route urls and query strings lowercase
-builder.Services.AddRouting(options =>
-{
+builder.Services.AddRouting(options => {
 	options.LowercaseUrls = true;
 	options.LowercaseQueryStrings = true;
 });
@@ -28,13 +36,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
 	app.UseSwagger();
 	app.UseSwaggerUI();
-}
-else
-{
+} else {
 	// Moved here because we don't need HTTPS in development
 	app.UseHttpsRedirection();
 }
