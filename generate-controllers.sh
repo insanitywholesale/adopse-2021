3f2338caa6c5105130e75d190a53b9198ab1596b
insanitywholesale <popomastor@gmail.com>
2022-03-30 14:32:09 +0300

    update controller gen script

--- generate-controllers.sh
+++ generate-controllers.sh
@@ -2,102 +2,91 @@
 
 echo "Controllers have already been generated, no need to run this"
 echo "If you know what you're doing, edit the script"
-#exit 1
+exit 1
 
 # delet dis
-rm Controllers/*
+#rm Controllers/*
 
 # evaluee answers
-dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name EvalueeAnswersController \
 	-m EvalueeAnswers \
 	-dc EvaluationContext \
 	-outDir Controllers
 # evaluation event
-dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name EvaluationEventController \
 	-m EvaluationEvent \
 	-dc EvaluationContext \
 	-outDir Controllers
 # evaluation
-dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name EvaluationController \
 	-m Evaluation \
 	-dc EvaluationContext \
 	-outDir Controllers
 # evaluee
-dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name EvalueeController \
 	-m Evaluee \
 	-dc EvaluationContext \
 	-outDir Controllers
 # evaluator
-dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name EvaluatorController \
 	-m Evaluator \
 	-dc EvaluationContext \
 	-outDir Controllers
 # organization
-dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name OrganizationController \
 	-m Organization \
 	-dc EvaluationContext \
 	-outDir Controllers
 # questions
-dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name EvaluationQuestionController \
 	-m EvaluationQuestion \
 	-dc EvaluationContext \
 	-outDir Controllers
-dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name OpenQuestionController \
 	-m OpenQuestion \
 	-dc EvaluationContext \
 	-outDir Controllers
 dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name MultipleChoiceQuestionController \
 	-m MultipleChoiceQuestion \
 	-dc EvaluationContext \
 	-outDir Controllers
 dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name QController \
 	-m Q \
 	-dc EvaluationContext \
 	-outDir Controllers
 # answers
-dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name EvaluationAnswerController \
 	-m EvaluationAnswer \
 	-dc EvaluationContext \
 	-outDir Controllers
-dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name OpenAnswerController \
 	-m OpenAnswer \
 	-dc EvaluationContext \
 	-outDir Controllers
-dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name MultipleChoiceAnswerController \
 	-m MultipleChoiceAnswer \
 	-dc EvaluationContext \
 	-outDir Controllers
-dotnet aspnet-codegenerator controller -async -api -f \
-	--no-build \
+dotnet aspnet-codegenerator controller -async -api \
 	-name AController \
 	-m A \
 	-dc EvaluationContext \
 	-outDir Controllers
+
 # needed cause otherwise there are a lot of whitespace changes
 dotnet format
