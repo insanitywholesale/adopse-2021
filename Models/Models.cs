557fbac147c8dc8e73cbaab87f9dab04cc7450b4
insanitywholesale <popomastor@gmail.com>
2022-04-02 16:42:26 +0300

    remove nullable shit

--- Models/Models.cs
+++ Models/Models.cs
@@ -1,62 +1,59 @@
+#nullable disable
 using System.ComponentModel.DataAnnotations;
 
 namespace adopse_2021.Models {
 	public class Person {
 		public long Id { get; set; }
-		public string? Name { get; set; }
-		public string? Email { get; set; }
-		public string? Phone { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
 	}
 
 	public class Evaluee : Person {
-		public string? Notes { get; set; }
+		public string Notes { get; set; }
 	}
 
 	public class Evaluator : Person {
-		public string? Organization { get; set; }
+		public string Organization { get; set; }
 	}
 
 	public class Evaluation {
 		public long Id { get; set; }
-		public string? Title { get; set; }
+		public string Title { get; set; }
 
-		//TODO: replace with Q
-		public virtual Q? Questions { get; set; }
+		public Q Questions { get; set; }
 	}
 
 	public class EvaluationEvent {
 		public long Id { get; set; }
 
 		[DataType(DataType.Date)]
-		public DateTime? Date { get; set; }
+		public DateTime Date { get; set; }
 
 		public bool Active { get; set; }
 		public bool Passed { get; set; }
 		public bool Completed { get; set; }
 
-		public Evaluation? Evaluation { get; set; }
+		public Evaluation Evaluation { get; set; }
 
-		public Evaluator? Evaluator { get; set; }
+		public Evaluator Evaluator { get; set; }
 
-		public virtual ICollection<EvalueeParticipation>? EvalueeParticipations { get; set; }
+		public ICollection<EvalueeParticipation> EvalueeParticipations { get; set; }
 	}
 
 	public class EvalueeParticipation {
 		public long Id { get; set; }
 
-		public Evaluee? Evaluee { get; set; }
+		public Evaluee Evaluee { get; set; }
 
-		public virtual A? Answers { get; set; }
-
-		public virtual long EvaluationEventId { get; set; }
-		public virtual EvaluationEvent? EvaluationEvent { get; set; }
+		public virtual A Answers { get; set; }
 	}
 
 	// For the inheritance stuff below I got help from here:
 	// https://stackoverflow.com/questions/66772647/how-to-deserialize-a-json-array-containing-objects-having-different-shape-in-c
 	public class EvaluationQuestion {
 		public long Id { get; set; }
-		public string? Heading { get; set; }
+		public string Heading { get; set; }
 	}
 
 	public class EvaluationAnswer {
@@ -65,35 +62,35 @@ namespace adopse_2021.Models {
 	}
 
 	public class OpenQuestion : EvaluationQuestion {
-		public OpenAnswer? Answer { get; set; }
+		public OpenAnswer Answer { get; set; }
 	}
 
 	public class OpenAnswer : EvaluationAnswer {
-		public string? ContentFromEvaluee { get; set; }
+		public string ContentFromEvaluee { get; set; }
 	}
 
 	public class MultipleChoiceQuestion : EvaluationQuestion {
 		public bool HasCorrectAnswer { get; set; }
-		public ICollection<MultipleChoiceAnswer>? Answers { get; set; }
+		public ICollection<MultipleChoiceAnswer> Answers { get; set; }
 	}
 
 	public class MultipleChoiceAnswer : EvaluationAnswer {
-		public string? Content { get; set; }
+		public string Content { get; set; }
 		public bool SelectedByEvaluee { get; set; }
 	}
 
 	public class Q {
 		public long Id { get; set; }
 
-		public ICollection<OpenQuestion>? OpenQuestions { get; set; }
-		public ICollection<MultipleChoiceQuestion>? MultipleChoiceQuestions { get; set; }
+		public ICollection<OpenQuestion> OpenQuestions { get; set; }
+		public ICollection<MultipleChoiceQuestion> MultipleChoiceQuestions { get; set; }
 	}
 
 	public class A {
 		public long Id { get; set; }
 
-		public ICollection<OpenAnswer>? OpenAnswers { get; set; }
-		public ICollection<MultipleChoiceAnswer>? MultipleChoiceAnswers { get; set; }
+		public ICollection<OpenAnswer> OpenAnswers { get; set; }
+		public ICollection<MultipleChoiceAnswer> MultipleChoiceAnswers { get; set; }
 	}
 
 	//TODO: add later
