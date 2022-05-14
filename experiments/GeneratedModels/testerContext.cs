14f38516ff0bfc491b5665d6c14cd6011cb1c8f8
insanitywholesale <popomastor@gmail.com>
2022-05-14 18:38:18 +0300

    comment out a line to silence useless warning

--- experiments/GeneratedModels/testerContext.cs
+++ experiments/GeneratedModels/testerContext.cs
@@ -28,7 +28,9 @@ namespace adopse_2021.experiments.GeneratedModels {
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
 			if (!optionsBuilder.IsConfigured) {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+				/*
+				#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+				*/
 				optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=tester;User Id=tester;Password=Apasswd");
 			}
 		}
