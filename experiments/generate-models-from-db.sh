6d9323bdc66b645ad72abc4bb00295d0c641bedf
insanitywholesale <popomastor@gmail.com>
2022-05-14 23:45:23 +0300

    delete experiments directory

--- experiments/generate-models-from-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-dotnet ef dbcontext scaffold \
-	"Server=localhost;Port=5432;Database=tester;User Id=tester;Password=Apasswd" \
-	Npgsql.EntityFrameworkCore.PostgreSQL \
-	-o GeneratedModels --force
